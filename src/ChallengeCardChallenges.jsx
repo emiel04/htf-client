a3bef77acfd485c91aef020281e39605ca1c52bd
cosmin <cosminsimion@outlook.be>
2024-11-19 14:55:32 +0100

    changed the code so you can navigate to a challange

--- src/ChallengeCardChallenges.jsx
+++ src/ChallengeCardChallenges.jsx
@@ -1,28 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBullseye } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
-function ChallengeCardChallenges({ title, xp, points, difficulty }) {
+function ChallengeCardChallenges({ id, title, xp, points, difficulty }) {
     return (
-        <motion.div
-            className="h-96 w-[calc((100%-2rem)/3)] flex-shrink-0 rounded-xl bg-base-100 flex flex-col items-center justify-center gap-6 p-4 shadow-2xl"
-            whileHover={{
-                scale: 1.05,
-                boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.2)",
-            }}
-            transition={{ type: "spring", stiffness: 300 }}
-        >
-            <FontAwesomeIcon icon={faBullseye} size="8x" className="text-black" />
-            <h2 className="text-2xl font-bold self-start">{title}</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam consequatur corporis expedita fuga hic
-            </p>
-            <div className="self-end flex gap-2">
-                <div className="badge badge-primary text-md font-bold p-2">{xp} xp</div>
-                <div className="badge badge-secondary text-md font-bold p-2">{points} pt</div>
-                <div className="badge badge-accent text-md font-bold p-2">{difficulty}</div>
-            </div>
-        </motion.div>
+        <Link to={`/challenge/${id}`} className="h-96 w-[calc((100%-2rem)/3)] flex-shrink-0 ">
+            <motion.div
+                className="rounded-xl bg-base-100 flex flex-col items-center justify-center gap-6 p-8 shadow-2xl cursor-pointer"
+                whileHover={{
+                    scale: 1.05,
+                    boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.2)",
+                }}
+                transition={{ type: "spring", stiffness: 300 }}
+            >
+                <FontAwesomeIcon icon={faBullseye} size="8x" className="text-black" />
+                <h2 className="text-2xl font-bold self-start">{title}</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam consequatur corporis expedita fuga hic
+                </p>
+                <div className="self-end flex gap-2">
+                    <div className="badge badge-primary text-md font-bold p-2">{xp} xp</div>
+                    <div className="badge badge-secondary text-md font-bold p-2">{points} pt</div>
+                    <div className="badge badge-accent text-md font-bold p-2">{difficulty}</div>
+                </div>
+            </motion.div>
+        </Link>
     );
 }
 
