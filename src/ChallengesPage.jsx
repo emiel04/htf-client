ca51c73170d0e8bf94769b6a7b678695c1e23efc
cosmin <cosminsimion@outlook.be>
2024-11-19 13:28:14 +0100

    early version challenges page

---
+++ src/ChallengesPage.jsx
@@ -0,0 +1,9 @@
+function ChallengesPage(){
+    return <div className="bg-blue-400 h-[calc(100vh-7rem)] flex">
+        <div className="h-80 w-80 bg-green-200">
+            dqs
+        </div>
+    </div>
+}
+
+export default ChallengesPage;
