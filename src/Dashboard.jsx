924f5019a5f930beb604f4c61156d83c82db0b8d
cosmin <cosminsimion@outlook.be>
2024-11-19 11:07:57 +0100

    started working on the navbar but will continue it later

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,6 +1,12 @@
 function Dashboard() {
     return <div className="h-[calc(100vh-7rem)] grid grid-rows-[0.5fr_1fr] grid-cols-2 gap-4">
-        <div className="bg-neutral shadow-xl  col-span-2 rounded-xl"></div>
+        <div className="bg-neutral shadow-xl  col-span-2 rounded-xl relative">
+            <div className="avatar absolute top-4 left-4">
+                <div className="w-48 rounded-full">
+                    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"/>
+                </div>
+            </div>
+        </div>
         <div className="bg-neutral  shadow-xl  rounded-xl"></div>
         <div className="shadow-xl  bg-neutral  rounded-xl"></div>
 
