462bfa1169eeeca0246c6b41855aaf5c99a9f7ff
cosmin <cosminsimion@outlook.be>
2024-11-19 10:17:00 +0100

    fixed router and created empty dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import Dashboard from "./dashboard/Dashboard.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 function App() {
-  return <div className="">Hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/about" element={<div>About Page</div>} />
+        <Route path="/contact" element={<div>Contact Page</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/dashboard/Dashboard.jsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <div>This is teh best dashboadr ever</div>;
+}
+
+export default Dashboard;
